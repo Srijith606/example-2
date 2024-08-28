841f11e6de3c950230268a6354b9343e5ec83f2b
Nishanth-2604 <167399433+Nishanth-2604@users.noreply.github.com>
2024-08-28 21:10:08 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <h1>Does this change the got file?
+    </h1>
   </React.StrictMode>
 );
 
